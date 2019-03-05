9b9ba055b70cfe5542405b3e8accb875ce8a73cc
Ennio Mara <enniomara@users.noreply.github.com>
2019-03-05 14:30:49 +0100

    Add documentation to APIService's findOneFull

--- src/app/cryptonator-api.service.ts
+++ src/app/cryptonator-api.service.ts
@@ -12,6 +12,10 @@ const API_URL = "https://api.cryptonator.com/api/";
 export class CryptonatorAPIService {
   constructor(private http: HttpClient) { }
 
+  /**
+   * Retrieve full information about a specific.
+   * @param id Id of the cryptocurrency, e.g. btc
+   */
   findOneFull(id: string): Observable<CryptonatorCrypto> {
     return this.http.get(API_URL + 'full/' + id + '-usd')
       .pipe(map((item: any) => new CryptonatorCrypto(item.ticker)));
