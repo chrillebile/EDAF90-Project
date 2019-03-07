1f1cea1dc871b28351c8a18a28e4bf263d2a40b3
Jonathan Jakobsson <jonathan.jakobsson2@gmail.com>
2019-03-07 14:35:57 +0100

    Formatting

--- src/app/conversion/currency.service.ts
+++ src/app/conversion/currency.service.ts
@@ -1,33 +1,33 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { map, catchError, tap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { map, catchError, tap } from "rxjs/operators";
 
-const endpoint1 = 'https://api.exchangeratesapi.io/';
-const endpoint2 = 'https://api.cryptonator.com/api/';
+const endpoint1 = "https://api.exchangeratesapi.io/";
+const endpoint2 = "https://api.cryptonator.com/api/";
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json'
+    "Content-Type": "application/json"
   })
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CurrencyService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-
-
-private extractData(res: Response) {
-  let body = res;
-  return body || { };
+  private extractData(res: Response) {
+    let body = res;
+    return body || {};
   }
 
-  private handleError<T> (operation = 'operation', result?: T) {
+  private handleError<T>(operation = "operation", result?: T) {
     return (error: any): Observable<T> => {
-
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
@@ -39,19 +39,21 @@ private extractData(res: Response) {
     };
   }
   getCurrencies(): Observable<any> {
-    return this.http.get(endpoint1 + 'latest?base=USD').pipe(
-      map(this.extractData));
+    return this.http
+      .get(endpoint1 + "latest?base=USD")
+      .pipe(map(this.extractData));
   }
   getCurrency(currencyCode): Observable<any> {
-    return this.http.get(endpoint1 + 'latest?base=USD').pipe(
-      map(this.extractData));
+    return this.http
+      .get(endpoint1 + "latest?base=USD")
+      .pipe(map(this.extractData));
   }
   getCryptos(): Observable<any> {
-    return this.http.get(endpoint2 + 'currencies').pipe(
-      map(this.extractData));
+    return this.http.get(endpoint2 + "currencies").pipe(map(this.extractData));
   }
   getCrypto(cryptoCode): Observable<any> {
-    return this.http.get(endpoint2 + 'ticker/' + cryptoCode +  '-usd').pipe(
-      map(this.extractData));
+    return this.http
+      .get(endpoint2 + "ticker/" + cryptoCode + "-usd")
+      .pipe(map(this.extractData));
   }
 }
