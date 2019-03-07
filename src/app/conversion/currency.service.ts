7e38eb8061c557d1a12366e458e866804ae15e22
Jonathan Jakobsson <jonathan.jakobsson2@gmail.com>
2019-03-07 15:15:36 +0100

    Update currency.service.ts

--- src/app/conversion/currency.service.ts
+++ src/app/conversion/currency.service.ts
@@ -1,19 +1,12 @@
 import { Injectable } from "@angular/core";
 import {
-  HttpClient,
-  HttpHeaders,
-  HttpErrorResponse
+  HttpClient
 } from "@angular/common/http";
-import { Observable, of } from "rxjs";
-import { map, catchError, tap } from "rxjs/operators";
+import { Observable} from "rxjs";
+import { map} from "rxjs/operators";
 
 const exchangeRatesAPI = "https://api.exchangeratesapi.io/";
 const cryptonatorAPI = "https://api.cryptonator.com/api/";
-const httpOptions = {
-  headers: new HttpHeaders({
-    "Content-Type": "application/json"
-  })
-};
 
 @Injectable({
   providedIn: "root"
@@ -24,19 +17,6 @@ export class CurrencyService {
   private extractData(res: Response) {
     let body = res;
     return body || {};
-  }
-
-  private handleError<T>(operation = "operation", result?: T) {
-    return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      console.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
   }
   getCurrencies(): Observable<any> {
     return this.http
