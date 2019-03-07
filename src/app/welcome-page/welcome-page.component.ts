bd4d21ca211bd911399b5dea0d9b19c5979e6630
Jonathan Jakobsson <jonathan.jakobsson2@gmail.com>
2019-03-07 14:58:58 +0100

    revert welcomepage to master

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,35 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
-@Component({
-  // selector: 'ngbd-table-basic',
-   templateUrl: './welcome-page.component.html',
-  // styleUrls: ['./welcome-page.component.css'],
-  })
-  export class WelcomePageComponent {
-    // elements: any = [
-    //   {id: 1, first: 'Mark', last: 'Otto', handle: '@mdo'},
-    //   {id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat'},
-    //   {id: 3, first: 'Larry', last: 'the Bird', handle: '@twitter'},
-    // ];
-
-    private baseUrl : string = 'https://api.cryptonator.com/api/ticker'
-    private products = [];
-    ngOnInit() {
-    }
-    get_btc_usd(){
-      this.httpClient.get(this.baseUrl).subscribe((res : any[])=>{
-      console.log(res);
-      this.products = res;
-      });
-  }
-    constructor(private httpClient: HttpClient){}
-  
-  }
-
-
-
-/*
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
@@ -43,4 +13,3 @@ export class WelcomePageComponent implements OnInit {
   }
 
 }
-*/
