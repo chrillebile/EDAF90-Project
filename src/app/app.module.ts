e23711fca62af29dfca70f32ea7ca9b38b3bf153
Ennio Mara <enniomara@users.noreply.github.com>
2019-03-12 13:18:24 +0100

    Revert back to using single quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
-import { HttpErrorResponse, HttpHandler } from "@angular/common/http";
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { NameComponent } from "./name/name.component";
-import { NavigationComponent } from "./navigation/navigation.component";
-import { WelcomePageComponent } from "./welcome-page/welcome-page.component";
-import { ConversionComponent } from "./conversion/conversion.component";
-import { FavoritesComponent } from "./favorites/favorites.component";
-import { SpecificInformationComponent } from "./specific-information/specific-information.component";
-import { CurrencyService } from "./conversion/currency.service";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from "@angular/forms";
-import { SpecificInformationCryptoComponent } from './specific-information-crypto/specific-information-crypto.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NameComponent } from './name/name.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ConversionComponent } from './conversion/conversion.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { SpecificInformationComponent } from './specific-information/specific-information.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient, HttpClientModule, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+import { SpecificInformationCryptoComponent } from './specific-information-crypto/specific-information-crypto.component';
+import { CurrencyService } from './conversion/currency.service';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -33,9 +31,11 @@ import { SpecificInformationCryptoComponent } from './specific-information-crypt
     NgbModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
   ],
-  providers: [CurrencyService],
+  providers: [
+    CurrencyService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
