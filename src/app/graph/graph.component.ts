7a755470cf68a729fe3c929c837c2ba2c6331934
Christian Bilevits <chrillebile@gmail.com>
2019-03-12 16:57:02 +0100

    Add graph component

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
-//import * as CanvasJS from '../../assets/canvasjs.min.js';
-import { CryptonatorCrypto } from '../cryptonator-crypto';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from '../../assets/canvasjs.min.js'
+import { CryptonatorAPIService } from '../cryptonator-api.service';
+import { getLocaleDateTimeFormat } from '@angular/common';
+
 
 @Component({
   selector: 'app-graph',
@@ -8,29 +11,62 @@ import { CryptonatorCrypto } from '../cryptonator-crypto';
   styleUrls: ['./graph.component.css']
 })
 export class GraphComponent implements OnInit {
-  //@Input() crypto : CryptonatorCrypto;
-  constructor() { }
+
+  private id: string;
+  private dataPoints: any;
+  private chart: any;
+
+  constructor(
+    private _crypto: CryptonatorAPIService,
+    private _route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    //let dataPoints = [];
-    //let dpsLength = 0;
-    /*let chart = new CanvasJS.Chart('chartContainer', {
+    this.id = this._route.snapshot.paramMap.get('id');
+    this.dataPoints = [];
+
+    this.chart = new CanvasJS.Chart('chartContainer', {
       exportEnabled: true,
       title: {
-        text: 'test'
+        text: this.id.toUpperCase() + ' - USD'
+      },
+      axisX: {
+        valueFormatString: "HH:mm:ss",
+      },
+      axisY: {
+        includeZero: false,
+        scaleBreaks: {
+          autoCalculate: true
+        },
       },
       data: [{
-        type: 'spline',
-        dataPoints
+        type: "spline",
+        dataPoints: this.dataPoints,
+        xValueType: "dateTime",
+        color: "#F08080",
       }]
     });
 
-    dataPoints.push({ x: 1, y: 2 });
-    dpsLength = dataPoints.length
-    chart.render()
-    updateChart()
-    function updateChart() {
+    function updateChart(graphComp) {
+      graphComp._crypto.findOneFull(graphComp.id).subscribe(crypto => {
+        graphComp.dataPoints.push({ x: (crypto.timestamp * 1000), y: parseInt(crypto.price) });
+        if (graphComp.dataPoints.length > 20) {
+          graphComp.dataPoints.shift();
+        }
+        graphComp.chart.render();
+      });
+    }
 
-    }*/
+    function createInterval(graphComp, interval) {
+      setInterval(
+        function () {
+          updateChart(graphComp);
+        },
+        interval
+      );
+    }
+    updateChart(this);
+    createInterval(this, 60000);
   }
+
 }
